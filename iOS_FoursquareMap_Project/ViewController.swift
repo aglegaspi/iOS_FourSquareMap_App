aa2367ed047b3519d2d2fe6521a3a4202aa470f8
aglegaspi <lex83nyc@gmail.com>
2019-11-06 16:34:48 -0500

    first commit

---
+++ iOS_FoursquareMap_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_FoursquareMap_Project
+//
+//  Created by Alex 6.1 on 11/6/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_FoursquareMap_ProjectTests/iOS_FoursquareMap_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_FoursquareMap_ProjectTests.swift
+//  iOS_FoursquareMap_ProjectTests
+//
+//  Created by Alex 6.1 on 11/6/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_FoursquareMap_Project
+
+class iOS_FoursquareMap_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
