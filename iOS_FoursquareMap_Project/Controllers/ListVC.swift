a4fd61230be8f944beefe007b76c54600444ee63
aglegaspi <lex83nyc@gmail.com>
2019-11-12 19:59:34 -0500

    annotations loading on mapview and set up ListVC

---
+++ iOS_FoursquareMap_Project/Controllers/ListVC.swift
@@ -0,0 +1,30 @@
+//
+//  ListVC.swift
+//  iOS_FoursquareMap_Project
+//
+//  Created by Alex 6.1 on 11/12/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS_FoursquareMap_Project/Networking/VenueAPIHelper.swift
@@ -0,0 +1,9 @@
+//
+//  VenueAPIHelper.swift
+//  iOS_FoursquareMap_Project
+//
+//  Created by Alex 6.1 on 11/12/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import Foundation
