39169ceac9bcc42f62c4f67a620b09bd41be3d36
aglegaspi <lex83nyc@gmail.com>
2020-02-19 07:11:09 -0500

    set up collection view cell

--- iOS_FoursquareMap_Project/Views/CollectionViewCell.swift
+++ iOS_FoursquareMap_Project/Views/CollectionViewCell.swift
@@ -9,5 +9,45 @@
 import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
+    static let reuseID = "CollectionCell"
+    
+    var collectionImage: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(systemName: "list.bullet")
+        return imageView
+    }()
+    
+    var collectionLabel: UILabel = {
+        var label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .callout)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews(collectionImage,collectionLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        collectionImage.translatesAutoresizingMaskIntoConstraints = false
+        collectionLabel.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 10
+        
+        NSLayoutConstraint.activate([
+            collectionImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+            collectionImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            collectionImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            collectionImage.heightAnchor.constraint(equalToConstant: 100),
+            
+            collectionLabel.topAnchor.constraint(equalTo: collectionImage.bottomAnchor, constant: 12),
+            collectionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            collectionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            collectionLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
     
 }
